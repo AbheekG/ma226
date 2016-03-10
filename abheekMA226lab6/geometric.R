b93a36ff64a0405a566ff00088258a883b3da427
Abheek Ghosh <abheekghosh.ag@gmail.com>
2016-03-10 10:44:15 +0530

    add geometric distribution

--- abheekMA226lab6/geometric.R
+++ abheekMA226lab6/geometric.R
@@ -9,8 +9,14 @@ u <- runif(n)
 for (i in 1:3)
 {
 	r <- as.integer(log(u)/log(q[i])) + 1
+	print(1-q[i])
 	print(r)
 	hist(r, main=paste("Geometric Distribution for about 50 values with p = ", 1-q[i]) , xlab="Range of random numbers", ylab="Density", breaks=50)
-	dev.copy(png, paste("plot1_",i,".png") )
+	if(i == 1)
+		dev.copy(png, "plot1_1.png")
+	if(i == 2)
+		dev.copy(png, "plot1_2.png")
+	if(i == 3)
+		dev.copy(png, "plot1_3.png")
 	dev.off ()
 }
