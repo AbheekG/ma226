9a7b8bc0158133c6c9da8c36f2fa45bba947c9d0
Abheek Ghosh <abheekghosh.ag@gmail.com>
2016-03-15 09:55:30 +0530

    implement mixed weibull distribution

--- abheekMA226lab6/weibull.R
+++ abheekMA226lab6/weibull.R
@@ -1,3 +1,30 @@
 weibull <- function(u, b, t) {
-	return ( exp( -log(log(1-u))/b - log(t) ))
+	return ( exp( (log(-log(1-u))/b) - log(t) ))
 }
+
+b1 <- 2
+t1 <- 1
+b2 <- 1.5
+t2 <- 1
+p <- 0.4
+
+n <- 50
+
+u1 <- runif(n)
+u2 <- runif(n)
+x <- vector(,n)
+
+for (i in 1:n)
+{
+	if(u1[i] < p) {
+		x[i] <- weibull(u2[i], b1, t1)
+	} else {
+		x[i] <- weibull(u2[i], b2, t2)
+	}
+}
+
+print(x)
+
+hist(x, main="Mixed Weibull Distribution, parameters (2, 1, 1.5, 1, 0.4), 50 values", xlab="Range of random numbers", ylab="Density")
+dev.copy(png,"plot3.png");
+dev.off ();
